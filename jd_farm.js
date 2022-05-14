0c63e6d323ae285cfed91f85ca646edfea399e69
huige <nginx11724@qq.com>
2022-05-14 17:21:20 +0800

    update

--- jd_farm.js
+++ jd_farm.js
@@ -1,6 +1,6 @@
 /*
 #City脚本配置
-cron "5 6-18/6 * * *" jd_farm.js, tag:东东农场日常任务
+cron "5 17 * * *" jd_farm.js, tag:东东农场日常任务
 */
 const $ = new Env('东东农场日常任务');
 let cookiesArr = [],
@@ -10,7 +10,8 @@ let message = '',
     subTitle = '',
     option = {},
     isFruitFinished = false;
-const retainWater = $.isNode() ? (process.env.retainWater ? process.env.retainWater : 100) : ($.getdata('retainWater') ? $.getdata('retainWater') : 100); //保留水滴大于多少g,默认100g;
+ // 保留水滴大于多少g,默认1000g;
+const retainWater = $.isNode() ? (process.env.retainWater ? process.env.retainWater : 1000) : ($.getdata('retainWater') ? $.getdata('retainWater') : 1000);
 let jdNotify = false; //是否关闭通知，false打开通知推送，true关闭通知推送
 let jdFruitBeanCard = false; //农场使用水滴换豆卡(如果出现限时活动时100g水换20豆,此时比浇水划算,推荐换豆),true表示换豆(不浇水),false表示不换豆(继续浇水),脚本默认是浇水
 let lnrun = 0;
